0327aa9f77ab0456aac8a55824be7d1043568471
emtwnty <muizahsanu@gmail.com>
2020-04-29 00:16:02 +0700

    fungsi cek user di praferences

--- app/src/main/java/com/example/sharedpreference/LoginActivity.kt
+++ app/src/main/java/com/example/sharedpreference/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.sharedpreference
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.sharedpreference.Model.UserModel
 import com.example.sharedpreference.SharedPreferences.Preferences
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,15 +14,42 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val username = et_username_login.text.toString()
-        val password = et_password_login.text.toString()
-        val userModel = UserModel(username,password,"")
+        // Ketika user menekan button masuk
+        btn_masuk_login.setOnClickListener(){
+            Login() // memanggil function Login()
+        }
 
-        val Preferences : Preferences = Preferences(baseContext)
-        Preferences.setUserPreferences(baseContext,userModel)
-        Preferences.setLoggedInStatus(baseContext,true)
+        // Ketika user menekan tombol daftar
+        tv_daftar_login.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
-        startActivity(Intent(baseContext,HomeActivity::class.java))
-        finish()
+    fun Login(){
+        // Deklarasi variabel
+        val username = et_username_login.text.toString() // mengambil value dari EditText username
+        val password = et_password_login.text.toString() // mengambil value dari EditText password
+        val Preferences = Preferences(baseContext) // Membuat object dari Class Preferences
+
+        // Pengecekan
+            // Apakah user sudah terdaftar atau belum
+        if(username.equals(Preferences.getRegisteredUser(baseContext)) // mengecek username dari Shared Preferences
+            && password.equals(Preferences.getRegisteredPassword(baseContext))){ //mengecek password dari Shared Preferences
+
+            val userModel = UserModel(username,password,"") // membuat objek dari UserModel dan menambahkan data
+
+            // Menyimpan data ke shared preferences
+            Preferences.setUserPreferences(baseContext,userModel) //
+            Preferences.setLoggedInStatus(baseContext,true)
+
+            // Pindah halaman ke home activity
+            startActivity(Intent(baseContext,HomeActivity::class.java))
+            finish()
+        }
+        else{ // kondisi jika user belum terdartar di shared preferences
+            Toast.makeText(this,"Belum Regis",Toast.LENGTH_LONG).show()
+        }
+        // END-Pengecekan
     }
 }
